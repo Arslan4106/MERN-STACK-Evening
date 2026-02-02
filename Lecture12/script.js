3658031be7a1bb9dbcfacdafbc25f1ccca916bf5
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-02-02 18:55:22 +0500

    Add files via upload

---
+++ Lecture12/script.js
@@ -0,0 +1,43 @@
+let firstName = "Jonas";
+let lastName = "Bob";
+console.log(firstName, lastName);
+// Rules to name a variable.
+// => You cannot use number at begining
+// let 2firstName, let firstName223;
+// => let first-Name (not Allowed), first_Name
+// => let first#Name (Not Allowed)
+// => let FirstName, (Capital Letter (Not Generate Errors but avoid to use it))
+// => Cannot Use Builtin Words (Pre-Defined)
+// console, if, for, function
+// let first Name;
+// let my name = "Imran Khan";
+let myName34 = 34;
+console.log(myName34);
+// Variable Declaration:
+let y;
+// Variable Initilization
+let z = 67;
+//
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "Pakistan";
+let continent = "Asia";
+let population = 344;
+console.log(country, continent, population);
+// Data Types:
+// Primptive and Non-Primptive
+// Primptive:
+// There are Seven Primptive Data Types in JavaScript
+//1. => Number (Decimal and Non-Decimal);
+// let x = 23.56; let y = 45;
+//2. => String: Characters Form
+// let firstName = "Imran";
+//3. => Boolean: (True and False);
+// e.g let f = 34 > 20 => True
+// e.g let k = 3 > 45 => False
+//4. undefined: Empty Value
+// e.g let n;
+// 5. null:  (having not a Worth)
+// let a = 45;
