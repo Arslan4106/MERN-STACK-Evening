337a4588c11392b035bef8785956a03162ce9e3a
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-02-23 15:55:18 +0500

    Add files via upload

---
+++ Lecture24/script.js
@@ -0,0 +1,6 @@
+// DOM Manipulation
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+// const secret =
+document.querySelector(".secretNumber").textContent = secretNumber;
+// console.log(secret);
