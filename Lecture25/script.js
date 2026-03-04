4aac9e6e70bb5c5d0a20822e5d3bf5b031bd76e4
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-03-04 15:32:33 +0500

    Add files via upload

--- Lecture25/script.js
+++ Lecture25/script.js
@@ -19,3 +19,12 @@ closeModal.addEventListener("click", function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 });
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (!modal.classList.contains("hidden")) {
+    if (e.key == "Escape") {
+      modal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    }
+  }
+});
